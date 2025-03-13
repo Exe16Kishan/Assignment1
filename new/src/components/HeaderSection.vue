88afd9f62a469f8193613e4c79e6e7f63c7f9852
Kishan <kishanshukla53@gmail.com>
2025-03-13 11:03:16 +0530

    fix#2

--- new/src/components/HeaderSection.vue
+++ new/src/components/HeaderSection.vue
@@ -2,7 +2,7 @@
     <header class="text-center mb-5">
       <h1>Event Management Portal</h1>
       <div class="my-4">
-        <img src="../../public/image.png" alt="Event Management" class="img-fluid rounded">
+        <img src="../assets/image.png" alt="Event Management" class="img-fluid rounded">
       </div>
     </header>
   </template>
